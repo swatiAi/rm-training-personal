2b0b247cc4094a3e96385715ec7062e251bf9cd7
Usman Swati <102903113+swatiAi@users.noreply.github.com>
2026-03-06 16:33:17 +0400

    Remove generated files and ignore build artifacts

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///D:/ResoluteProjects/abrm/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///D:/ResoluteProjects/abrm/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
